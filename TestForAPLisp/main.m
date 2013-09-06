b4c93cb52ed1a2ef504ecd38f7d741a309401f0f
Andrei Puni <andrei.puni@yardi.com>
2013-09-06 11:29:00 +0300

    Initial Commit

---
+++ TestForAPLisp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestForAPLisp
+//
+//  Created by Andrei Puni on 9/6/13.
+//  Copyright (c) 2013 Andrei Puni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
